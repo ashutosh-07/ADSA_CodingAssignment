9e7d12fe80e679738bd34898aa4d1c95226c3f40
ashutosh-07WS <ashutosharma07@gmail.com>
2026-09-21 00:35:22 +0530

    Red black tree changes

---
+++ Q3.cpp
@@ -0,0 +1,246 @@
+#include <iostream>
+using namespace std;
+#include <queue>
+
+// Structure of the node
+struct Node
+{
+    int data;
+    bool color;
+    struct Node *left;
+    struct Node *right;
+    struct Node *parent;
+
+    Node(int value)
+    {
+        color = 1; //1 for red 0 for black
+        data = value;
+        left = NULL;
+        right = NULL;
+        parent = NULL;
+    }
+};
+
+Node *search(Node *root, int key)
+{
+
+    while (root != NULL)
+    {
+        if (root->data == key)
+        {
+            
+            return root;
+            // found the key
+        }
+        else if (root->data > key)
+        {
+            root = root->left;
+        }
+        else
+        {
+            root = root->right;
+        }
+    }
+    return NULL;
+}
+
+Node *insert(Node *root, int key)
+{
+
+    if (root == NULL)
+    {
+        int color;       // RED / BLACK
+        Node *root = new Node(key);
+        return root;
+    }
+    Node *temp = root;
+    Node *prev = NULL;
+    while (temp != NULL)
+    {
+        prev = temp;
+        if (temp->data > key)
+        {
+            // cout<<"root is greater going left"<<endl;
+            temp = temp->left;
+        }
+        else if (temp->data < key)
+        {
+            // cout<<"root is smaller going right"<<endl;
+            temp = temp->right;
+        }
+        else
+        {
+            cout << "Duplicate key";
+            return root;
+        }
+    }
+    Node *newNode = new Node(key);
+
+    if (key > prev->data)
+    {
+        prev->right = newNode;
+        newNode->parent = prev;
+    }
+    else
+    {
+        prev->left = newNode;
+        newNode->parent = prev;
+    }
+
+    return root;
+}
+
+void printInOrder(Node *root)
+{
+    if (root == NULL)
+        return;
+
+    printInOrder(root->left);
+
+    cout << root->data << " ";
+
+    printInOrder(root->right);
+}
+void printPreOrder(Node *root)
+{
+    if (root == NULL)
+        return;
+
+    cout << root->data << " ";
+
+    printPreOrder(root->left);
+    printPreOrder(root->right);
+}
+void printLevelOrder(Node *root)
+{
+    queue<vector<Node *>> q;
+
+    vector<Node *> v;
+    v.push_back(root);
+    q.push(v);
+
+    while (!q.empty())
+    {
+        vector<Node *> v = q.front();
+        vector<Node *> res;
+        q.pop();
+        for (int i = 0; i < v.size(); i++)
+        {
+            Node *temp = v[i];
+
+            cout << temp->data << " ";
+            if (temp->left != NULL)
+            {
+                res.push_back(temp->left);
+            }
+            if (temp->right != NULL)
+            {
+                res.push_back(temp->right);
+            }
+        }
+
+        if (res.size() > 0)
+        {
+            q.push(res);
+        }
+        cout << endl;
+    }
+}
+void printStructure(Node *root)
+{
+    cout << "INORDER: ";
+    printInOrder(root);
+    cout << endl;
+    cout << "PREORDER: ";
+    printPreOrder(root);
+    cout << endl;
+    cout << "LEVELORDER: " << endl;
+    printLevelOrder(root);
+    cout << "END" << endl;
+}
+
+int main()
+{
+    Node *root;
+
+    // Build Initial_BST
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        int key;
+        cin >> key;
+        // call function to insert this key
+        if (i == 0)
+        {
+            // root = insert(NULL, key);
+        }
+        else
+        {
+            // root = insert(root, key);
+        }
+    }
+
+    cout << "INITIAL TREE" << endl;
+    printStructure(root);
+
+    // Search
+    int S;
+    cin >> S;
+    for (int i = 0; i < S; i++)
+    {
+        int key;
+        cin >> key;
+        // call function to search this key
+        cout << "SEARCH " << key << ": ";
+        if(search(root, key)==NULL){
+            cout << "Key not found" << endl;
+        }
+        else{
+            cout << "Key found" << endl;
+        }
+    }
+
+    // Insert
+    // int I;
+    // cin >> I;
+    // for (int i = 0; i < I; i++)
+    // {
+    //     int key;
+    //     cin >> key;
+    //     // call function to Insert this key
+    //     cout << "INSERT " << key << endl;
+    //     if (search(root, key)==NULL)
+    //     {
+    //         root  = insert(root, key);
+    //         printStructure(root);
+            
+    //     }
+    //     else
+    //     {
+    //         cout << "Duplicate key" << endl;
+    //     }
+    // }
+    // Delete
+
+    //Comment down for TC 1
+    // int D;
+    // cin >> D;
+
+    // for (int i = 0; i < D; i++)
+    // {
+    //     int key;
+    //     cin >> key;
+    //     // call function to Insert this key
+    //     cout << "DELETE " << key << endl;
+    //     if (search(root, key)!=NULL)
+    //     {
+    //         root = deleteNode(root, key);
+    //         printStructure(root);
+    //     }
+    //     else
+    //     {
+    //         cout << "Key not found" << endl;
+    //     }
+    // }
+}
--- README.md
+++ README.md
@@ -4,3 +4,9 @@ This repository contains my coding assignments for ADSA.
 
 Q1 -> BST Operations (Search, Insert, Delete) and Traversals
 Q2 -> AVL Tree Operations(Search, Insert{handeling Rotations RR, RL, LR, LL}, Delete) and Traversals
+Q3 -> Red Black tree has few important points:
+1. Node can only be of two colors red or black
+2. Root and NIL(they are node Node but we consider them as Black Node) node is always black
+3. All Path from a node to NILL discedent has equal number of black nodes
+4. Red node can't have red children
+5. The longest path(root to furtherest NILL) <= 2 x The shortlest path(Root to nearest NIL) Shortest path: all black nodes, Longest path: Black mixed with red nodes.
